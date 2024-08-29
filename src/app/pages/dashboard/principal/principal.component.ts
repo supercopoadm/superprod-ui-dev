6513018ab83bea719f3bd78d10d5bbca9f4aaa91
SuperCopoADM <injexprodadm@gmail.com>
2024-08-29 15:42:45 -0300

    Update principal.component.ts

--- src/app/pages/dashboard/principal/principal.component.ts
+++ src/app/pages/dashboard/principal/principal.component.ts
@@ -25,7 +25,7 @@ export class PrincipalComponent implements OnInit {
       );
     } else {
       this.urlMetabase = this.sanitizer.bypassSecurityTrustResourceUrl(
-        `http://localhost:2000/public/dashboard/4358c4d1-7a2d-4a4d-8fc0-4e6c6dd1cf5e`,
+        `http://localhost:3000/public/dashboard/85962085-b650-4778-97ad-92034192d223`,
       );
     }
   }
